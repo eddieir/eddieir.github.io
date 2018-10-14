95290b765584a73acb7f5377cccdc4c597e04c38
eddieir <peyman.iravani@gmail.com>
2018-10-14 13:45:22 +0200

    Updates

---
+++ precache-manifest.d9d92fbf12226daab6e569e9199de4e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3cddabf299075348cec2",
+    "url": "/peymaniravaniportofolio/static/js/runtime~main.3cddabf2.js"
+  },
+  {
+    "revision": "4ed7869f4b870175bb0f",
+    "url": "/peymaniravaniportofolio/static/js/main.4ed7869f.chunk.js"
+  },
+  {
+    "revision": "7eae6140ad0adf38fbdc",
+    "url": "/peymaniravaniportofolio/static/js/1.7eae6140.chunk.js"
+  },
+  {
+    "revision": "4ed7869f4b870175bb0f",
+    "url": "/peymaniravaniportofolio/static/css/main.4e1e987f.chunk.css"
+  },
+  {
+    "revision": "cc4d1d36ee6443092df09b21da5144e4",
+    "url": "/peymaniravaniportofolio/index.html"
+  }
+];
